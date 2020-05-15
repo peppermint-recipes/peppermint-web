65b2809fafecc15655290a175c777a6129d9dfcf
theErikss0n <erik@der-klinger.de>
2020-05-15 17:14:30 +0200

    Send requests to backend

--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -1,25 +1,25 @@
 import http from '../plugins/axios';
 
 export const createRecipe = async (recipe) => {
-  const response = await http.post('/recipes', recipe);
+  const response = await http.post('/recipe', recipe);
 
-  return response.data;
+  return response.data.data;
 };
 
 export const getAllRecipes = async () => {
-  const response = await http.get('/recipes');
+  const response = await http.get('/recipe');
 
-  return response.data;
+  return response.data.data;
 };
 
 export const getRecipeById = async (id) => {
-  const response = await http.get(`/recipes/${id}`);
+  const response = await http.get(`/recipe/${id}`);
 
-  return response.data;
+  return response.data.data;
 };
 
 export const deleteRecipe = async (id) => {
-  const response = await http.delete(`/recipes/${id}`);
+  const response = await http.delete(`/recipe/${id}`);
 
-  return response.data;
+  return response.data.data;
 };
